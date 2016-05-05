bdede594f0e8a9b62406e86f07716ead969c897c
liuzhuo <971066723@qq.com>
2016-05-05 21:04:47 +0800

    study

---
+++ out/production/scala/txt.scala
@@ -0,0 +1,28 @@
+import java.io.File
+
+import scala.io.Source
+
+object txt {
+  def main(args: Array[String]) {
+
+    //    for (line <- Source.fromFile("D:\\scala\\src\\main\\scala\\txt.scala", "UTF-8").getLines()) {
+    //      println(line.length + " " + line)
+    //    }
+
+    val lines = Source.fromFile("D:\\scala\\src\\main\\scala\\txt.scala", "UTF-8").getLines().toList
+
+    val longestLine = lines.reduceLeft(
+      (a, b) => if (a.length > b.length) a else b
+    )
+    val maxWidth = widthOfLength(longestLine)
+    for (line <- lines) {
+      val numSpace = maxWidth - widthOfLength (line)
+      val padding = " " * numSpace
+      println(padding + line.length + "|" + line)
+    }
+  }
+
+  def widthOfLength(s: String) = s.length.toString.length
+
+
+}
--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,7 @@
 import java.math.BigInteger
 
+//import scala.collection.mutable.Map
+
 /**
   * Created by liuzhuo on 2016/4/28.
   *
@@ -12,29 +14,45 @@ import java.math.BigInteger
   * @since [产品/模块版本]
   */
 
- object HelloWorld  {
-     def main(args: Array[String]) {
-//       args.foreach(arg=>print(arg+"\t"))
-//       args.foreach(println)
-//       for(arg <- args) println(arg)
-       val big = new Integer("123");
-       val greetStrings = new  Array[String](3)
-       greetStrings(0)="hello"
-       greetStrings(1)=","
-       greetStrings(2)=String.valueOf(big)
-       greetStrings.foreach(println)
-        println("-----------------------------")
+object HelloWorld {
+  def main(args: Array[String]) {
+    //       args.foreach(arg=>print(arg+"\t"))
+    //       args.foreach(println)
+    //       for(arg <- args) println(arg)
+    val big = new Integer("123");
+    val greetStrings = new Array[String](3)
+    greetStrings(0) = "hello"
+    greetStrings(1) = ","
+    greetStrings(2) = String.valueOf(big)
+    greetStrings.foreach(println)
+    println("-----------------------------")
 
-       val oneTwo = List(1,2);
-       val threeFour = List(3,4);
-       val oneTwoTrheeFour = oneTwo:::threeFour
-       println(oneTwo+"and"+threeFour+"were not mutated")
-       println("Thus: "+oneTwoTrheeFour+"is a new List")
-       println("-----------------------------")
+    val oneTwo = List(1, 2);
+    val threeFour = List(3, 4);
+    val oneTwoTrheeFour = oneTwo ::: threeFour
+    println(oneTwo + "and" + threeFour + "were not mutated")
+    println("Thus: " + oneTwoTrheeFour + "is a new List")
+    println("-----------------------------")
 
-       val pair = (99,"元组")
-       println(pair._1)
-       println(pair._2)
+    val pair = (99, "元组")
+    println(pair._1)
+    println(pair._2)
+    println("-----------------------------")
 
-       }
+    var jetSet = Set("xiaoming", "xiaohong")
+    jetSet += "xiaoli"
+    println(jetSet.contains("xiaoli"))
+
+    println("-----------------------------")
+    var treasureMap = Map[Int, String]()
+    treasureMap += (1 -> "Go to island")
+    treasureMap += (2 -> "Find X")
+    treasureMap += (3 -> "Dig")
+    println(treasureMap(2))
+    println("-----------------------------")
+
+    val romanMap = Map(1->'a',2->123,3->"c")
+    println(romanMap(2))
+
+  }
 }

b14f3ce12672bbd9d90b8549e400d220741001a5
liuzhuo <971066723@qq.com>
2016-05-18 14:30:32 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	src/main/scala/Rational.scala

---
+++ src/main/scala/LongLines.scala
@@ -0,0 +1,18 @@
+/**
+  * Created by liuzhuo on 2016/5/18.
+  *
+  * &lt;一句话功能简述&gt;
+  * &lt;功能详细描述&gt;
+  *
+  * @author liuzhuo
+  * @version [版本号, ${date}]
+  * @see [相关类/方法]
+  * @since [产品/模块版本]
+  */
+
+object  LongLines{
+
+  def processFile(filename:String,width:Int): Unit ={
+    val source = Source
+  }
+}
